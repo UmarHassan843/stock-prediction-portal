ffc39a903f65f47af2968876fd5392beb497e5ae
Umar Hassan <umarhassan843@gmail.com>
2025-12-10 00:37:05 +0100

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,6 +3,8 @@ import './assets/css/style.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Register from './components/Register'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 
 
 
@@ -12,9 +14,15 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+    <BrowserRouter>
+    <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<div>Login</div>} />
+        </Routes>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
